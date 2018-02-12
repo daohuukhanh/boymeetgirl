3cd3b4e946421370c47b592ed8c0dcc23de8ad26
daohuukhanh <daohuukhanh@outlook.com>
2018-02-12 12:22:11 -0800

    second commit

---
+++ bin/html5/haxe/ManifestResources.hx
@@ -0,0 +1,128 @@
+package;
+
+
+import lime.app.Config;
+import lime.utils.AssetLibrary;
+import lime.utils.AssetManifest;
+import lime.utils.Assets;
+
+#if sys
+import sys.FileSystem;
+#end
+
+@:access(lime.utils.Assets)
+
+
+@:keep @:dox(hide) class ManifestResources {
+	
+	
+	public static var preloadLibraries:Array<AssetLibrary>;
+	public static var preloadLibraryNames:Array<String>;
+	
+	
+	public static function init (config:Config):Void {
+		
+		preloadLibraries = new Array ();
+		preloadLibraryNames = new Array ();
+		
+		var rootPath = null;
+		
+		if (config != null && Reflect.hasField (config, "rootPath")) {
+			
+			rootPath = Reflect.field (config, "rootPath");
+			
+		}
+		
+		if (rootPath == null) {
+			
+			#if (ios || tvos || emscripten)
+			rootPath = "assets/";
+			#elseif (sys && windows && !cs)
+			rootPath = FileSystem.absolutePath (haxe.io.Path.directory (#if (haxe_ver >= 3.3) Sys.programPath () #else Sys.executablePath () #end)) + "/";
+			#else
+			rootPath = "";
+			#end
+			
+		}
+		
+		Assets.defaultRootPath = rootPath;
+		
+		#if (openfl && !flash && !display)
+		
+		#end
+		
+		var data, manifest, library;
+		
+		data = '{"name":null,"assets":"aoy4:pathy26:data%2Fgraphics%2Fbeam.pngy4:sizei288y4:typey5:IMAGEy2:idR1y7:preloadtgoR0y25:data%2Fgraphics%2Fboy.pngR2i1672R3R4R5R7R6tgoR0y26:data%2Fgraphics%2Fboy2.pngR2i1459R3R4R5R8R6tgoR0y28:data%2Fgraphics%2Fflower.pngR2i1463R3R4R5R9R6tgoR0y30:data%2Fgraphics%2Fgameover.pngR2i7082R3R4R5R10R6tgoR0y26:data%2Fgraphics%2Fgirl.pngR2i2450R3R4R5R11R6tgoR0y27:data%2Fgraphics%2Fgirl2.pngR2i2316R3R4R5R12R6tgoR0y28:data%2Fgraphics%2Fhearts.pngR2i720R3R4R5R13R6tgoR0y29:data%2Fgraphics%2Fhearts1.pngR2i465R3R4R5R14R6tgoR0y29:data%2Fgraphics%2Fhearts2.pngR2i608R3R4R5R15R6tgoR0y25:data%2Fgraphics%2Fsun.pngR2i2122R3R4R5R16R6tgoR0y26:data%2Fgraphics%2Fsun1.pngR2i1893R3R4R5R17R6tgoR0y26:data%2Fgraphics%2Fsun2.pngR2i1863R3R4R5R18R6tgoR0y25:data%2Fgraphics%2Fwin.pngR2i9712R3R4R5R19R6tgoR0y34:data%2Fhow%20to%20add%20assets.txtR2i7117R3y4:TEXTR5R20R6tgoR0y15:data%2Ficon.pngR2i143966R3R4R5R22R6tgh","version":2,"libraryArgs":[],"libraryType":null}';
+		manifest = AssetManifest.parse (data, rootPath);
+		library = AssetLibrary.fromManifest (manifest);
+		Assets.registerLibrary ("default", library);
+		
+		
+		library = Assets.getLibrary ("default");
+		if (library != null) preloadLibraries.push (library);
+		else preloadLibraryNames.push ("default");
+		
+		
+	}
+	
+	
+}
+
+
+#if !display
+#if flash
+
+@:keep @:bind #if display private #end class __ASSET__data_graphics_beam_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_boy_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_boy2_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_flower_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_gameover_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_girl_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_girl2_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_hearts_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_hearts1_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_hearts2_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_sun_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_sun1_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_sun2_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_graphics_win_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__data_how_to_add_assets_txt extends null { }
+@:keep @:bind #if display private #end class __ASSET__data_icon_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind #if display private #end class __ASSET__manifest_default_json extends null { }
+
+
+#elseif (desktop || cpp)
+
+@:image("data/graphics/beam.png") #if display private #end class __ASSET__data_graphics_beam_png extends lime.graphics.Image {}
+@:image("data/graphics/boy.png") #if display private #end class __ASSET__data_graphics_boy_png extends lime.graphics.Image {}
+@:image("data/graphics/boy2.png") #if display private #end class __ASSET__data_graphics_boy2_png extends lime.graphics.Image {}
+@:image("data/graphics/flower.png") #if display private #end class __ASSET__data_graphics_flower_png extends lime.graphics.Image {}
+@:image("data/graphics/gameover.png") #if display private #end class __ASSET__data_graphics_gameover_png extends lime.graphics.Image {}
+@:image("data/graphics/girl.png") #if display private #end class __ASSET__data_graphics_girl_png extends lime.graphics.Image {}
+@:image("data/graphics/girl2.png") #if display private #end class __ASSET__data_graphics_girl2_png extends lime.graphics.Image {}
+@:image("data/graphics/hearts.png") #if display private #end class __ASSET__data_graphics_hearts_png extends lime.graphics.Image {}
+@:image("data/graphics/hearts1.png") #if display private #end class __ASSET__data_graphics_hearts1_png extends lime.graphics.Image {}
+@:image("data/graphics/hearts2.png") #if display private #end class __ASSET__data_graphics_hearts2_png extends lime.graphics.Image {}
+@:image("data/graphics/sun.png") #if display private #end class __ASSET__data_graphics_sun_png extends lime.graphics.Image {}
+@:image("data/graphics/sun1.png") #if display private #end class __ASSET__data_graphics_sun1_png extends lime.graphics.Image {}
+@:image("data/graphics/sun2.png") #if display private #end class __ASSET__data_graphics_sun2_png extends lime.graphics.Image {}
+@:image("data/graphics/win.png") #if display private #end class __ASSET__data_graphics_win_png extends lime.graphics.Image {}
+@:file("data/how to add assets.txt") #if display private #end class __ASSET__data_how_to_add_assets_txt extends haxe.io.Bytes {}
+@:image("data/icon.png") #if display private #end class __ASSET__data_icon_png extends lime.graphics.Image {}
+@:file("") #if display private #end class __ASSET__manifest_default_json extends haxe.io.Bytes {}
+
+
+
+#else
+
+
+
+#end
+
+#if (openfl && !flash)
+
+
+
+#end
+#end
---
+++ src/Main.hx
@@ -0,0 +1,181 @@
+import haxegon.*;
+
+class Main
+{
+
+	var x1 = 100;
+	var y1 = 200;
+
+	var x2 = Gfx.screenheight;
+	var y2 = 200;
+
+	var x3 = Gfx.screenwidthmid;
+	var y3 = Gfx.screenheightmid;
+
+	var x4 = Gfx.screenwidthmid;
+	var y4 = Gfx.screenheightmid;
+
+	var x5 = Gfx.screenwidthmid;
+	var y5 = Gfx.screenheightmid;
+
+	var speed  = 5;
+	var flowerspeed  = 10;
+	var beamspeed  = 15;
+
+	var count = 0;
+	var score = 0;
+	var up = false;
+	var lives = 3;
+	var win = 10;
+
+	function init()
+	{
+
+	}
+
+	function update()
+	{
+		Gfx.fillbox(0, 0, Gfx.screenwidth, Gfx.screenheight, Col.WHITE);
+
+		if (lives == 3) Gfx.drawimage(0, 0, "hearts");
+		if (lives == 2) Gfx.drawimage(0, 0, "hearts2");
+		if (lives == 1) Gfx.drawimage(0, 0, "hearts1");
+		if (lives == 0)
+		{
+			Gfx.fillbox(0, 0, Gfx.screenwidth, Gfx.screenheight, Col.WHITE);
+			Gfx.drawimage(100,  100, "gameover");
+		}
+
+		if (score == win)
+		{
+			Gfx.fillbox(0, 0, Gfx.screenwidth, Gfx.screenheight, Col.WHITE);
+			Gfx.drawimage(0,  0, "win");
+		}
+
+		if (lives > 0 && score < win)
+		{
+			if (count % 15 == 0)
+			{
+				Gfx.drawimage(x1, y1, "boy");
+			}
+			else
+			{
+				Gfx.drawimage(x1, y1, "boy2");
+			}
+
+			Gfx.drawimage(x2, y2, "girl");
+			Gfx.drawimage(x3, y3, "flower");
+
+			Gfx.drawimage(x4, y4, "beam");
+			Gfx.drawimage(x5, y5, "beam");
+
+			if (count % 3 == 0)
+			{
+				Gfx.drawimage(Gfx.screenwidth - 200, 0, "sun");
+			}
+			else if (count % 3 == 1)
+			{
+				Gfx.drawimage(Gfx.screenwidth - 200, 0, "sun1");
+			}
+			else
+			{
+				Gfx.drawimage(Gfx.screenwidth - 200, 0, "sun2");
+			}
+
+			if (Input.pressed(Key.LEFT))
+			{
+				x1 -= speed;
+			}
+
+			if (Input.pressed(Key.RIGHT))
+			{
+				x1 += speed;
+			}
+
+			if (Input.pressed(Key.UP))
+			{
+				y1 -= speed;
+			}
+
+			if (Input.pressed(Key.DOWN))
+			{
+				y1 += speed;
+			}
+
+			if (x1 < -20) x1 = -20;
+			if (y1 < -50) y1 = -50;
+
+			if (x1 > Gfx.screenwidth - 200) x1 = Gfx.screenwidth - 200;
+			if (y1 > Gfx.screenheight - 300) y1 = Gfx.screenheight - 300;
+
+			if (x2 < -20) x2 = -20;
+			if (y2 < -50) y2 = -50;
+
+			if (x2 > Gfx.screenwidth - 200) x2 = Gfx.screenwidth - 200;
+			if (y2 > Gfx.screenheight - 300) y2 = Gfx.screenheight - 500;
+
+			if (Math.abs(x1 - x2) < 20 && Math.abs(y1 - y2) < 20)
+			{
+				score++;
+				x2 = Random.int(0, Gfx.screenwidth - 20);
+				y2 = Random.int(0, Gfx.screenheight - 50);
+			}
+
+			if (!up)
+			{
+				y3 -= flowerspeed;
+
+				if (y3 < 0)
+				{
+					up = true;
+				}
+			}
+
+			if (up)
+			{
+				y3 += flowerspeed;
+
+				if (y3 == Gfx.screenheight - 100)
+				{
+					up = false;
+				}
+
+			}
+
+			if (x3 > x1 && x3 + 106 < x1 + 165 && y3 < y1 && y3 - 138 > y1 - 257)
+			{
+				lives--;
+				x1 = 0;
+				y1 = 0;
+			}
+
+			if (x4 == x1 +165 && y4 > y1 && y4 + 37 < y1 + 257)
+			{
+				lives--;
+				x1 = 0;
+				y1 = 0;
+			}
+
+			if (x5 + 37 == x1 && y5 > y1 && y4 + 37 < y5 + 257)
+			{
+				lives--;
+				x1 = 0;
+				y1 = 0;
+			}
+
+			x4 -= beamspeed;
+			x5 += beamspeed;
+
+			y4 = y3;
+			y5 = y3;
+
+			if (x4 < 0) x4 = Gfx.screenwidthmid;
+			if (x5 > Gfx.screenwidth - 50) x5 = Gfx.screenwidthmid;
+
+			Text.size = 4;
+			Text.display(Text.RIGHT, Text.BOTTOM, "SCORE: " + score, Col.BLACK, 1);
+
+			count++;
+		}
+	}
+}
